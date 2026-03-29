9fdba7ef35eee5d7494455f0a79418f4aa1b5fdc
SergeyStBer <sergeystman@gmail.com>
2026-03-29 23:33:56 +0300

    Create 20240327000000_create_workflow_states.rb

---
+++ db/migrate/20240327000000_create_workflow_states.rb
@@ -0,0 +1,17 @@
+# db/migrate/20240327000000_create_workflow_states.rb
+
+class CreateWorkflowStates < ActiveRecord::Migration[7.0]
+  def change
+    create_table :workflow_states do |t|
+      t.integer :record_id, null: false
+      t.string :workflow_name, null: false
+      t.string :state
+      t.text :data
+      t.json :transitions, default: []
+      
+      t.timestamps
+    end
+    
+    add_index :workflow_states, [:record_id, :workflow_name], unique: true
+  end
+end
